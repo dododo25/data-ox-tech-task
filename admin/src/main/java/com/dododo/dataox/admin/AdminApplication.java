e344d807526e0514833836cc79b96b8f11fb974c
dododo25 <dmytro.terekhov@outlook.com>
2025-08-13 12:01:44 +0300

    Update main classes

--- admin/src/main/java/com/dododo/dataox/admin/AdminApplication.java
+++ admin/src/main/java/com/dododo/dataox/admin/AdminApplication.java
@@ -2,12 +2,21 @@ package com.dododo.dataox.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories
-@ComponentScan({"com.dododo.dataox.core"})
+@EnableJpaRepositories(basePackages = {
+        "com.dododo.dataox.core.repository"
+})
+@EntityScan(basePackages = {
+        "com.dododo.dataox.core.model"
+})
+@ComponentScan(basePackages = {
+        "com.dododo.dataox.core",
+        "com.dododo.dataox.admin"
+})
 public class AdminApplication {
 
     public static void main(String[] args) {
